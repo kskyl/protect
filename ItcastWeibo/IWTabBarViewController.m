347ec9e9a0503cab5d08b05d8d0f41d471b2fcdf
kskyl <1466053659@qq.com>
2015-08-19 15:01:23 +0800

    this is a protect

---
+++ ItcastWeibo/IWTabBarViewController.m
@@ -0,0 +1,71 @@
+//
+//  IWTabBarViewController.m
+//  ItcastWeibo
+//
+//  Created by apple on 14-5-5.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import "IWTabBarViewController.h"
+#import "IWHomeViewController.h"
+#import "IWMessageViewController.h"
+#import "IWMeViewController.h"
+#import "IWDiscoverViewController.h"
+
+@interface IWTabBarViewController ()
+
+@end
+
+@implementation IWTabBarViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // 初始化所有的子控制器
+    [self setupAllChildViewControllers];
+}
+
+/**
+ *  初始化所有的子控制器
+ */
+- (void)setupAllChildViewControllers
+{
+    // 1.首页
+    IWHomeViewController *home = [[IWHomeViewController alloc] init];
+    [self setupChildViewController:home title:@"首页" imageName:@"tabbar_home" selectedImageName:@"tabbar_home_selected"];
+    
+    // 2.消息
+    IWMessageViewController *message = [[IWMessageViewController alloc] init];
+    [self setupChildViewController:message title:@"消息" imageName:@"tabbar_message_center" selectedImageName:@"tabbar_message_center_selected"];
+    
+    // 3.广场
+    IWDiscoverViewController *discover = [[IWDiscoverViewController alloc] init];
+    [self setupChildViewController:discover title:@"广场" imageName:@"tabbar_discover" selectedImageName:@"tabbar_discover_selected"];
+    
+    // 4.我
+    IWMeViewController *me = [[IWMeViewController alloc] init];
+    [self setupChildViewController:me title:@"我" imageName:@"tabbar_profile" selectedImageName:@"tabbar_profile_selected"];
+}
+
+/**
+ *  初始化一个子控制器
+ *
+ *  @param childVc           需要初始化的子控制器
+ *  @param title             标题
+ *  @param imageName         图标
+ *  @param selectedImageName 选中的图标
+ */
+- (void)setupChildViewController:(UIViewController *)childVc title:(NSString *)title imageName:(NSString *)imageName selectedImageName:(NSString *)selectedImageName
+{
+    // 1.设置控制器的属性
+    childVc.title = title;
+    childVc.tabBarItem.image = [UIImage imageNamed:imageName];
+    childVc.tabBarItem.selectedImage = [[UIImage imageNamed:selectedImageName] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    
+    // 2.包装一个导航控制器
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:childVc];
+    [self addChildViewController:nav];
+}
+
+@end
